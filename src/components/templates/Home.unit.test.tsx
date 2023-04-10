cbaf147235d1ec723ccb675ae5aedaa7d9cd27f6
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-04-10 14:35:32 +0900

    feat: use generic Stub type

--- src/components/templates/Home.unit.test.tsx
+++ src/components/templates/Home.unit.test.tsx
@@ -4,28 +4,36 @@ import React from "react";
 import Home from "@/components/templates/Home";
 import { UserTracker } from "@/domain/model/UserTracker";
 
-describe("Home", () => {
-  let trackStub: {
-    track: UserTracker["track"];
-    event?: Parameters<UserTracker["track"]>[0];
+type Stub<F extends (...args: any) => any> = {
+  f: F;
+  args?: Parameters<F>;
+};
+
+const stub = <F extends (...args: any) => any>(): Stub<F> => {
+  const s: Stub<F> = {
+    f: ((...args: any): any => {
+      s.args = args;
+    }) as F,
   };
 
+  return s;
+};
+
+describe("Home", () => {
+  let trackStub: Stub<UserTracker["track"]>;
+
   beforeEach(() => {
-    trackStub = {
-      track: (event) => {
-        trackStub.event = event;
-      },
-    };
+    trackStub = stub();
   });
 
-  it("should call track function with 'home:product-tour-button:click' argument when '무료 체험 시작하' button is clicked.", () => {
+  it("should call track function with 'home:product-tour-button:click' argument when '무료 체험 시작하기' button is clicked.", () => {
     // given
-    const rendered = render(<Home track={trackStub.track} />);
+    const rendered = render(<Home track={trackStub.f} />);
 
     // when
     rendered.getByText("무료 체험 시작하기").click();
 
     //then
-    expect(trackStub.event).toBe("home:product-tour-button:click");
+    expect(trackStub.args?.[0]).toBe("home:product-tour-button:click");
   });
 });
