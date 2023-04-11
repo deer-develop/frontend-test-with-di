531b9afa26a1f5814135d3bed8ecb10f508af5ab
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-04-11 21:13:09 +0900

    feat: rename stub to spy

--- src/components/templates/Home.unit.test.tsx
+++ src/components/templates/Home.unit.test.tsx
@@ -5,13 +5,13 @@ import Home from "@/components/templates/Home";
 import { UserTracker } from "@/domain/model/UserTracker";
 import LinkStub from "@/domain/model/LinkStub";
 
-type Stub<F extends (...args: any) => any> = {
+type Spy<F extends (...args: any) => any> = {
   f: F;
   args?: Parameters<F>;
 };
 
-const stub = <F extends (...args: any) => any>(): Stub<F> => {
-  const s: Stub<F> = {
+const spy = <F extends (...args: any) => any>(): Spy<F> => {
+  const s: Spy<F> = {
     f: ((...args: any): any => {
       s.args = args;
     }) as F,
@@ -21,21 +21,21 @@ const stub = <F extends (...args: any) => any>(): Stub<F> => {
 };
 
 describe("Home", () => {
-  let trackStub: Stub<UserTracker["track"]>;
+  let trackSpy: Spy<UserTracker["track"]>;
 
   beforeEach(() => {
-    trackStub = stub();
+    trackSpy = spy();
   });
 
   it("should call track function with 'home:product-tour-button:click' argument when '무료 체험 시작하기' button is clicked.", () => {
     // given
-    const rendered = render(<Home Link={LinkStub} track={trackStub.f} />);
+    const rendered = render(<Home Link={LinkStub} track={trackSpy.f} />);
 
     // when
     rendered.getByText("무료 체험 시작하기").click();
 
     //then
-    expect(trackStub.args?.[0]).toBe("home:product-tour-button:click");
+    expect(trackSpy.args?.[0]).toBe("home:product-tour-button:click");
   });
 
   it("should call track function with 'home:apply-button:click' argument when '사용 신청하기' button is clicked.", () => {
